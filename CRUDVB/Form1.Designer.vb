19fba22ab81a6a29f6c20118b5983422ed56a37f
Miguel2236 <miguelm2236@gmail.com>
2021-01-20 13:50:12 -0600

    CRUD simple en visual basic

---
+++ CRUDVB/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUser
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.cmbCity = New System.Windows.Forms.ComboBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.rdbMale = New System.Windows.Forms.RadioButton()
+        Me.rdbFemale = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Arial", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.Maroon
+        Me.lblTitle.Location = New System.Drawing.Point(116, 23)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(200, 27)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "CRUD User Form"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(34, 88)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(26, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ID"
+        '
+        'txtID
+        '
+        Me.txtID.Enabled = False
+        Me.txtID.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtID.Location = New System.Drawing.Point(121, 82)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(195, 26)
+        Me.txtID.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(34, 219)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "City"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(34, 175)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 20)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Address"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(34, 131)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Name"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(33, 313)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(36, 20)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Sex"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(34, 265)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(38, 20)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "Age"
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(121, 125)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(195, 26)
+        Me.txtName.TabIndex = 8
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAddress.Location = New System.Drawing.Point(121, 172)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(195, 26)
+        Me.txtAddress.TabIndex = 9
+        '
+        'txtAge
+        '
+        Me.txtAge.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAge.Location = New System.Drawing.Point(121, 265)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(195, 26)
+        Me.txtAge.TabIndex = 10
+        '
+        'cmbCity
+        '
+        Me.cmbCity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbCity.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmbCity.FormattingEnabled = True
+        Me.cmbCity.Items.AddRange(New Object() {"Colima", "Guadalajara", "Queretaro", "Monterrey"})
+        Me.cmbCity.Location = New System.Drawing.Point(121, 219)
+        Me.cmbCity.Name = "cmbCity"
+        Me.cmbCity.Size = New System.Drawing.Size(195, 28)
+        Me.cmbCity.TabIndex = 13
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(171, 368)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(88, 38)
+        Me.btnAdd.TabIndex = 14
+        Me.btnAdd.Text = "ADD"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'rdbMale
+        '
+        Me.rdbMale.AutoSize = True
+        Me.rdbMale.Location = New System.Drawing.Point(121, 315)
+        Me.rdbMale.Name = "rdbMale"
+        Me.rdbMale.Size = New System.Drawing.Size(48, 17)
+        Me.rdbMale.TabIndex = 15
+        Me.rdbMale.TabStop = True
+        Me.rdbMale.Text = "Male"
+        Me.rdbMale.UseVisualStyleBackColor = True
+        '
+        'rdbFemale
+        '
+        Me.rdbFemale.AutoSize = True
+        Me.rdbFemale.Location = New System.Drawing.Point(216, 315)
+        Me.rdbFemale.Name = "rdbFemale"
+        Me.rdbFemale.Size = New System.Drawing.Size(59, 17)
+        Me.rdbFemale.TabIndex = 16
+        Me.rdbFemale.TabStop = True
+        Me.rdbFemale.Text = "Female"
+        Me.rdbFemale.UseVisualStyleBackColor = True
+        '
+        'frmUser
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(413, 430)
+        Me.Controls.Add(Me.rdbFemale)
+        Me.Controls.Add(Me.rdbMale)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.cmbCity)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "frmUser"
+        Me.Text = "User"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents cmbCity As ComboBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents rdbMale As RadioButton
+    Friend WithEvents rdbFemale As RadioButton
+End Class
---
+++ CRUDVB/Form1.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Data.SqlClient
+
+Public Class frmUser
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        ' Dim id As Integer = txtID.Text
+        Dim name As String = txtName.Text
+        Dim address As String = txtAddress.Text
+        Dim city As String = cmbCity.Text
+        Dim age As Integer = txtAge.Text
+        Dim sex As String = ""
+
+        If rdbMale.Checked = True Then
+            sex = rdbMale.Text
+        ElseIf rdbFemale.Checked = True Then
+            sex = rdbFemale.Text
+        End If
+
+        'se realiza la consulta de captura y la configuracion de la base de datos
+        Dim query As String = "INSERT INTO UserInfo(Name,Address,city,Age,Sex) VALUES(@name,@address,@city,@age,@sex)"
+        Using con As SqlConnection = New SqlConnection("Data Source=DESKTOP-JT4DFR6;Initial Catalog=Users;Integrated Security=True")
+            Using cmd As SqlCommand = New SqlCommand(query, con)
+                'cmd.Parameters.AddWithValue("@id", id)
+                cmd.Parameters.AddWithValue("@name", name)
+                cmd.Parameters.AddWithValue("@address", address)
+                cmd.Parameters.AddWithValue("@city", city)
+                cmd.Parameters.AddWithValue("@age", age)
+                cmd.Parameters.AddWithValue("@sex", sex)
+                con.Open()
+                cmd.ExecuteNonQuery()
+                con.Close()
+                clear_form()
+                MessageBox.Show("Successfully added")
+            End Using
+        End Using
+    End Sub
+
+    Private Sub clear_form()
+        ' Limpiar formulario
+        txtName.Text = ""
+        txtAddress.Text = ""
+        cmbCity.Items.Clear()
+        txtAge.Text = ""
+        rdbMale.Checked = False
+        rdbFemale.Checked = False
+    End Sub
+
+End Class
